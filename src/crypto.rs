c5ad5d43961e9adaf7dad89ddd3ab12ba08b8415
Lluís Oriol Colom Nichols <lluis.colom@borderless-technologies.com>
2025-11-17 19:52:20 +0100

    Check openssl command status code

--- src/crypto.rs
+++ src/crypto.rs
@@ -13,9 +13,16 @@ use std::process::Command;
 pub fn ec_params(nid: Nid, output_file: &str) -> Result<(), anyhow::Error> {
     let curve_name = nid.long_name()?;
     // No available rust bindings, so use the CLI
-    Command::new("openssl")
+    let output = Command::new("openssl")
         .args(["ecparam", "-name", curve_name, "-out", output_file])
         .output()?;
+
+    if !output.status.success() {
+        anyhow::bail!(
+            "Failed to generate EC parameters: {}",
+            String::from_utf8_lossy(&output.stderr)
+        );
+    }
     Ok(())
 }
 
