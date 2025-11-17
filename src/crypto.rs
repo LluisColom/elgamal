a31a0da6afde38e43e4d0e9b2d7fd30d73d40a59
Lluís Oriol Colom Nichols <lluis.colom@borderless-technologies.com>
2025-11-17 14:23:29 +0100

    Implement encryption, hmac and secret processing

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,4 +1,10 @@
+use openssl::hash::MessageDigest;
 use openssl::nid::Nid;
+use openssl::pkey::PKey;
+use openssl::rand::rand_bytes;
+use openssl::sha::Sha256;
+use openssl::sign::Signer;
+use openssl::symm::{Cipher, encrypt};
 use std::process::Command;
 
 pub fn ec_params(nid: Nid, output_file: &str) -> Result<(), anyhow::Error> {
@@ -36,5 +42,66 @@ pub fn gen_pub_key(input: &str, output: &str) -> Result<(), anyhow::Error> {
             String::from_utf8_lossy(&output.stderr)
         );
     }
+    Ok(())
+}
+
+pub fn gen_secret(inkey: &str, peerkey: &str, output: &str) -> Result<(), anyhow::Error> {
+    let output = Command::new("openssl")
+        .args(&[
+            "pkeyutl", "-derive", "-inkey", inkey, "-peerkey", peerkey, "-out", output,
+        ])
+        .output()?;
+
+    if !output.status.success() {
+        anyhow::bail!(
+            "Failed to extract pub key: {}",
+            String::from_utf8_lossy(&output.stderr)
+        );
+    }
+    Ok(())
+}
+
+pub fn process_secret(input: &str) -> Result<Vec<u8>, anyhow::Error> {
+    // Read the shared secret
+    let shared_secret = std::fs::read(input)?;
+    // Hash the shared secret
+    let mut hasher = Sha256::new();
+    hasher.update(shared_secret.as_slice());
+    let digest = hasher.finish();
+    Ok(digest.to_vec())
+}
+
+pub fn encryption(key: &[u8], input: &str) -> Result<(), anyhow::Error> {
+    // Read the plain text
+    let plaintext = std::fs::read(input)?;
+
+    if key.len() != 32 {
+        anyhow::bail!("Encryption key must be 32 bytes long");
+    }
+
+    // Generate a random IV
+    let mut iv = [0u8; 16];
+    rand_bytes(&mut iv)?;
+
+    // Encrypt with AES-128-CBC
+    let cipher = Cipher::aes_128_cbc();
+    let ciphertext = encrypt(cipher, &key[..16], Some(&iv), plaintext.as_slice())?;
+
+    // Prepare data for HMAC
+    let mut mac_data = Vec::with_capacity(key.len() + ciphertext.len());
+    mac_data.extend_from_slice(&iv);
+    mac_data.extend_from_slice(&ciphertext);
+
+    // Generate SHA256-HMAC
+    let hmac_key = PKey::hmac(&key[16..])?;
+    let mut signer = Signer::new(MessageDigest::sha256(), &hmac_key)?;
+    signer.update(&mac_data)?;
+    let hmac = signer.sign_to_vec()?;
+
+    // Export the ciphertext, IV and tag
+    std::fs::write("ciphertext.bin", ciphertext)?;
+    std::fs::write("iv.bin", iv)?;
+    std::fs::write("tag.bin", hmac)?;
+
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ const PRIV_FILE: &str = "priv.pem";
 const PUB_FILE: &str = "pub.pem";
 const EPH_PRIV_FILE: &str = "ephpkey.pem";
 const EPH_PUB_FILE: &str = "ephpubkey.pem";
+const SECRET: &str = "secret";
 
 #[derive(Parser, Debug)]
 #[command(author, version, about)]
@@ -21,7 +22,7 @@ enum Command {
     /// EC parameters and key generation
     Param,
     /// Encrypts a document
-    Encrypt,
+    Encrypt { peer_key: String, document: String },
     /// Decrypts a document
     Decrypt,
     /// Verify a proof
@@ -42,11 +43,17 @@ fn main() -> Result<(), anyhow::Error> {
             crypto::gen_pub_key(PRIV_FILE, PUB_FILE)?;
             println!("Parameter generation successful");
         }
-        Command::Encrypt => {
+        Command::Encrypt { peer_key, document } => {
             // ensure!();
             // Generate a new ephemeral keypair
             crypto::gen_priv_key(PARAM_FILE, EPH_PRIV_FILE)?;
-
+            crypto::gen_pub_key(PRIV_FILE, EPH_PUB_FILE)?;
+            // Derive the secret using ECDH
+            crypto::gen_secret(EPH_PRIV_FILE, peer_key.as_str(), SECRET)?;
+            // Extract session key from secret
+            let key = crypto::process_secret(SECRET)?;
+            // Encrypt the document + HMAC
+            crypto::encryption(&key, document.as_str())?;
             println!("Document encryption successful");
         }
         Command::Decrypt => {
