3e09de49d9b9f1b078e3233a10739ff11fa45369
Lluís Oriol Colom Nichols <lluis.colom@borderless-technologies.com>
2025-11-18 11:30:36 +0100

    Enhance EC group CLI

--- src/cli.rs
+++ src/cli.rs
@@ -13,15 +13,10 @@ pub fn choose_functionality() -> Result<Command, anyhow::Error> {
 }
 
 pub fn choose_group() -> Result<Nid, anyhow::Error> {
-    let ec_groups = vec![
-        (Nid::SECP256K1, "secp256k1", "Bitcoin/Ethereum"),
-        (Nid::X9_62_PRIME256V1, "prime256v1", "NIST P-256"),
-        (Nid::BRAINPOOL_P256R1, "brainpoolP256r1", "Brainpool"),
-    ];
-
     let nid = select("Select EC group:")
-        .items(&ec_groups)
-        .initial_value(Nid::SECP256K1)
+        .item(Nid::SECP256K1, "secp256k1", "Bitcoin/Ethereum")
+        .item(Nid::X9_62_PRIME256V1, "prime256v1", "NIST P-256")
+        .item(Nid::BRAINPOOL_P256R1, "brainpoolP256r1", "Brainpool")
         .interact()?;
 
     Ok(nid)
